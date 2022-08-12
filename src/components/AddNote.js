067846bd456fc341186df32bb9741cb8a3bdad02
amrutapalo <amrutapalo1997@gmail.com>
2022-08-12 14:53:53 +0530

    some design changes

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -5,7 +5,8 @@ const AddNote = () => {
     <form className="m-3">
       <div className="mb-3">
         <h3>Add a node</h3>
-        <input type="email" className="form-control form-control-lg" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+        <input type="text" className="form-control form-control-lg p-3 my-3" placeholder='Title'/>
+        <input type="text" className="form-control form-control-lg p-3 my-3" placeholder='Description'/>
         {/* <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div> */}
       </div> 
       <button type="submit" className="btn btn-primary">Submit</button>
